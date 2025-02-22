26b3ddc6437789bca0d08f532dcf39581fab6e65
Santiago Vega <sjvega1789@gmail.com>
2025-02-22 20:29:25 -0300

    add new lock entry to powermenu

--- .config/fuzzel/scripts/powermenu.sh
+++ .config/fuzzel/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown\n Lock"
 
 selected=$(echo -e $entries|fuzzel --config=/home/jim/.config/fuzzel/config --lines=4 -d -p ?| awk '{print tolower($2)}')
 
@@ -13,4 +13,6 @@ case $selected in
     exec systemctl reboot;;
   shutdown)
     exec systemctl poweroff -i;;
+  lock)
+    exec pkill -SIGUSR1 swayidle;;
 esac
